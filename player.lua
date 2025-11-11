f93f8dfe48fad2e600151fc403b5486b4ef151fb
ShrimpCatDev <shrimpcatboi223@outlook.com>
2025-11-11 07:05:07 -0600

    add player movement

--- player.lua
+++ player.lua
@@ -2,8 +2,20 @@ local e=require("entity")
 
 local pl=e:extend()
 
+function pl:new(x,y)
+    pl.super.new(self,x,y,12,12)
+    self.spd=50
+    self.vx=0
+    self.vy=0
+end
+
 function pl:update(dt)
-    self.x=self.x+dt*12
+    local x,y=input:get("move")
+    self.vx=x*self.spd*dt
+    self.vy=y*self.spd*dt
+
+    local ax,ay,col,len=world:move(self,self.x+self.vx,self.y+self.vy)
+    self.x,self.y=ax,ay
 end
 
 function pl:draw()
