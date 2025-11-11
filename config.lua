5babe325cf0d96483724576ab29ea7fdad2d461c
ShrimpCatDev <shrimpcatboi223@outlook.com>
2025-11-11 06:59:16 -0600

    update input file

--- config.lua
+++ config.lua
@@ -13,10 +13,13 @@ conf.vsync=true
 
 conf.input={
     controls={
-
+        left = {'key:left', 'key:a', 'axis:leftx-', 'button:dpleft'},
+        right = {'key:right', 'key:d', 'axis:leftx+', 'button:dpright'},
+        up = {'key:up', 'key:w', 'axis:lefty-', 'button:dpup'},
+        down = {'key:down', 'key:s', 'axis:lefty+', 'button:dpdown'},
     },
     pairs={
-
+        move = {'left', 'right', 'up', 'down'}
     },
     joystick = love.joystick.getJoysticks()[1],
 }
